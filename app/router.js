40b94d421767cde2cb02e124b65cc17378feb288
Jonathan Lee <Jonathan_Lee@abercrombie.com>
2017-10-04 11:55:05 -0400

    Added a bunch of expected routes.

--- app/router.js
+++ app/router.js
@@ -7,6 +7,31 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('player', function() {
+    this.route('equipment', {
+      path: ':player_id'
+    });
+
+    this.route('items', {
+      path: ':player_id'
+    });
+  });
+
+  this.route('shop', {
+    path: ':shop_id'
+  }, function() {
+    this.route('index', {
+      path: ':shop_id'
+    });
+
+    this.route('buy', {
+      path: ':shop_id'
+    });
+
+    this.route('sell', {
+      path: ':shop_id'
+    });
+  });
 });
 
 export default Router;
